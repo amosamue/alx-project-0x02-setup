80ff7d6cc6853abb2e23e37d8be1652d39ff4c5f
amosamue <samuelamoh130@gmail.com>
2025-09-22 02:03:28 +0000

    Card: use type import and alias; Home: import Card via @/components/common/Card

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,14 +1,12 @@
 // components/common/Card.tsx
 import React from "react";
-import { CardProps } from "../../interfaces";
+import { type CardProps } from "@/interfaces";
 
-const Card: React.FC<CardProps> = ({ title, content }) => {
+export default function Card({ title, content }: CardProps) {
   return (
     <div className="border rounded-lg shadow-md p-4 bg-white">
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <p className="text-gray-700">{content}</p>
     </div>
   );
-};
-
-export default Card;
+}
